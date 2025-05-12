bc0d1c747be596cb407e3eb25ea8886d1588ea05
Parth Dhoot <69670124+ParthDhoot27@users.noreply.github.com>
2025-05-12 12:20:59 +0530

    Add files via upload

---
+++ PROJECTS/recursion/recursion(intermediate and advance)/towerofhanoi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class towerofhanoi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of blocks on first tower : ");
+        int n = sc.nextInt();
+        System.out.println("NOTE: tower are numbered from top to down and 1st and 3rd pillar are source  and destination respectively!");
+        towerofhanoi(n,"1", "2", "3");
+    }
+
+    public static void towerofhanoi(int n, String src, String hel, String dest) {
+
+        if (n==1) {
+            System.out.println("transfer disk "+ n +" from tower "+ src + " to tower " + dest);
+            return; 
+        }
+        towerofhanoi(n-1, src , dest, hel);
+        System.out.println("transfer disk "+ n +" from tower "+ src + " to tower " + dest);
+        towerofhanoi(n-1, hel, src, dest);
+    } 
+    
+}
