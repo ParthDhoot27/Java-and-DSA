c94cfba42e8c089c7c47ef5422711c42b36462d1
Parth Dhoot <69670124+ParthDhoot27@users.noreply.github.com>
2025-05-13 13:32:18 +0530

    Updated occurance.java

--- PROJECTS/recursion/recursion(intermediate and advance)/occurance.java
+++ PROJECTS/recursion/recursion(intermediate and advance)/occurance.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 public class occurance {
     
     public static void occurance(String smp, int i, int first, int last , char target) {
+        //base case 
         if (i==smp.length()) {
-            // first += 1;
-            // last += 1;
+            first += 1;
+            last += 1;
             System.out.println("the first occourance of "+ target + " is at "+ first);    
             System.out.println("the last occourance of "+ target + " is at "+ last);    
             return;
         }
         char b = smp.charAt(i);
-        if (b == 'a' && i<=first) {
+        //assigning index to of first occurance to first 
+        if (b == target && i<=first) {
             first = i;
-        }else if (i>= first && b == 'a') {
+            last  = first ;
+        }
+        //assigning index to of all other occurances to last 
+        else if (i>= first && b == target ) {
             last= i;
         }
+        //recursion for analysing all values
         occurance(smp, i+1 , first, last, target);
     }    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
