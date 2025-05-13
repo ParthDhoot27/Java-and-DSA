b207057f3ee847d3c2a7ce2cba3975236ea47449
Parth Dhoot <69670124+ParthDhoot27@users.noreply.github.com>
2025-05-13 10:48:53 +0530

    Add files via upload

---
+++ PROJECTS/recursion/recursion(intermediate and advance)/occurance.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class occurance {
+    
+    public static void occurance(String smp, int i, int first, int last , char target) {
+        if (i==smp.length()) {
+            // first += 1;
+            // last += 1;
+            System.out.println("the first occourance of "+ target + " is at "+ first);    
+            System.out.println("the last occourance of "+ target + " is at "+ last);    
+            return;
+        }
+        char b = smp.charAt(i);
+        if (b == 'a' && i<=first) {
+            first = i;
+        }else if (i>= first && b == 'a') {
+            last= i;
+        }
+        occurance(smp, i+1 , first, last, target);
+    }    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("type any word or sentence ");
+        String random = sc.nextLine();
+        System.out.println("enter the character whose first and last occourance you want to find ?");
+        String targetinp = sc.next();
+        if (targetinp.length()> 1) {
+            
+        }
+        char target = targetinp.charAt(0);
+        int lnth = random.length();
+        occurance(random, 0, lnth , 0, target ); 
+    }
+}
