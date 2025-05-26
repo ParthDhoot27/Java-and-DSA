0c8e34ec08ebc17a2a16f6a3d3b8053ad177e8e8
Parth Dhoot <69670124+ParthDhoot27@users.noreply.github.com>
2025-05-26 08:55:18 +0530

    Add files via upload
    this program prints all the possible subsecuences of a given string without repeating same combinations ( using hashset's in java)

--- PROJECTS/recursion/recursion(intermediate and advance)/subsequences.java
+++ PROJECTS/recursion/recursion(intermediate and advance)/subsequences.java
@@ -1,21 +1,34 @@
 
 import java.util.Scanner;
+
+import java.util.HashSet;
+
 public class subsequences {
-    public static void combs(String  demo, String finalstr, int idx) {
+    public static void combs(String  demo, String finalstr, int idx, HashSet<String> set) {
+
+        //hashset 
         if (idx == demo.length()) {
-            System.out.println(finalstr);
-            return;
+            
+            // for printing unique sequences (using hashset)
+            if (set.contains(finalstr)) {
+                return;            
+            }else{
+                System.out.println(finalstr);
+                set.add(finalstr);
+                return;
+            }
         }
         // to get into string
-        combs(demo,finalstr+demo.charAt(idx), idx+1);
+        combs(demo,finalstr+demo.charAt(idx), idx+1, set);
 
         // to not get into string 
-        combs(demo,finalstr,idx+1);
+        combs(demo,finalstr,idx+1,set);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String demo = sc.next();
-        combs(demo,"", 0);
+        HashSet<String> set = new HashSet<>();
+        combs(demo,"", 0, set);
     }
 }
 // in the output a blank space is also printed which is the case in which all the letters 
